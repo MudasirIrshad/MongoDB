8ad7bba1c1796f4fb9f10c651a016a5e7b83badd
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-02-01 21:00:54 +0500

    Mongoose

--- TodoApp/index.js
+++ TodoApp/index.js
@@ -6,7 +6,9 @@ const mongoose=require('mongoose')
 const connectionStr='mongodb+srv://mudasirirshad47:mudasir123456789@cluster0.jzcnrjw.mongodb.net/TodoApp'
 mongoose.connect(connectionStr)
 
-
+app.get('/addtask',(req,res)=>{
+    app.send('add data here functionality')
+})
 app.listen(port,()=>{
     console.log("Server started on port: "+port);
 })
