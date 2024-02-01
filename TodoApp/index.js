ab482a4f168fd068fccdb04b03d2365d63e42f72
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-02-01 20:07:29 +0500

    Moongose connected

--- TodoApp/index.js
+++ TodoApp/index.js
@@ -2,9 +2,9 @@ const express=require('express')
 const app=express()
 const bodyparser=require('body-parser')
 const port=3000
-
-
-
+const mongoose=require('mongoose')
+const connectionStr='mongodb+srv://mudasirirshad47:mudasir123456789@cluster0.jzcnrjw.mongodb.net/TodoApp'
+mongoose.connect(connectionStr)
 
 
 app.listen(port,()=>{
