4315528f5ba7eac98e362df11df62f8ffb898f32
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-02-03 23:54:31 +0500

    Define todo schema

--- TodoApp/index.js
+++ TodoApp/index.js
@@ -2,13 +2,21 @@ const express=require('express')
 const app=express()
 const bodyparser=require('body-parser')
 const port=3000
-const mongoose=require('mongoose')
-const connectionStr='mongodb+srv://mudasirirshad47:mudasir123456789@cluster0.jzcnrjw.mongodb.net/TodoApp'
-mongoose.connect(connectionStr)
+const mongoose = require('mongoose')
+const url='mongodb+srv://mudasirirshad47:mudasir123456789@cluster0.jzcnrjw.mongodb.net/TodoApp'
+mongoose.connect(url)
 
-app.get('/addtask',(req,res)=>{
-    app.send('add data here functionality')
+const Tasks=new mongoose.Schema({
+    title:String,
+    description:String
 })
+
+const AddTask=mongoose.model('AddTask',Tasks)
+
+app.post('/tasks',(req,res)=>{
+    res.send('Tasks added successfully')
+})
+
 app.listen(port,()=>{
-    console.log("Server started on port: "+port);
+    console.log(`Server is running on port ${port}`)
 })
