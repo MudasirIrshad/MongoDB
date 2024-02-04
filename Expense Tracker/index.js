a40dfe88e8d28714a853732c11cb5b0c0d192081
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-02-05 01:10:56 +0500

    User Authentication

--- Expense Tracker/index.js
+++ Expense Tracker/index.js
@@ -17,19 +17,28 @@ const UserSignupSchema=new mongoose.Schema({
 const UserSignup=mongoose.model('UserSignup',UserSignupSchema)
 
 const userKey="UserSignup Done"
-app.post('/user/signup',(req, res) => {
+app.post('/user/signup',async(req, res) => {
     const {name,gmail,password}=req.body
-    const newUser=new UserSignup({
-        name,
-        gmail,
-        password
-    })
-    console.log(req.body);
-    newUser.save()
-    const token=jwt.sign({name,gmail,password},userKey)
-    if(newUser){
-        res.send(token)
+    const foundUser=await UserSignup.findOne({gmail})
+    console.log(foundUser);
+    if(foundUser){
+        res.send('User Already Exist')
     }
+    else{
+        const newUser=new UserSignup({
+            name,
+            gmail,
+            password
+        })
+        console.log(req.body);
+        newUser.save()
+        const token=jwt.sign({name,gmail,password},userKey)
+        if(newUser){
+            res.send(token)
+        }
+    }
+    
+    
 })
 
 app.post('/user/login',(req, res) => {
